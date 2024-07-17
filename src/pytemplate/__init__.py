87b27073a3aec55b800b0da16777259ba51a3534
ByteOtter <christopher-hock@protonmail.com>
2024-07-17 12:04:52 +0200

    Add pre-commit and format code

--- src/pytemplate/__init__.py
+++ src/pytemplate/__init__.py
@@ -4,4 +4,3 @@ This file indicates the main package of your application.
 Every package of your application needs a __init__.py file. It *can* be empty,
 or contain code.
 """
-
--- src/pytemplate/addition/addition.py
+++ src/pytemplate/addition/addition.py
@@ -2,4 +2,4 @@ def add(a: float, b: float) -> float:
     """
     Add two floats together.
     """
-    return a+b
+    return a + b
